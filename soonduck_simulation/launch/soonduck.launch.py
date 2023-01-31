a8d9061fc279a8ed28dfcfe00567447c289e0913
sxj754 <sxj754@case.edu>
2023-01-31 15:11:29 -0500

    Seperate robot_state_publisher launch file

--- soonduck_simulation/launch/soonduck.launch.py
+++ soonduck_simulation/launch/soonduck.launch.py
@@ -7,8 +7,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-import xacro
-
 
 def generate_launch_description():
     world_file = os.path.join(
@@ -30,24 +28,17 @@ def generate_launch_description():
         launch_arguments={"world": world_file}.items(),
     )
 
-    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
-
-    xacro_file = os.path.join(
-        get_package_share_directory("soonduck_simulation"),
-        "description",
-        "soonduck.urdf.xacro",
-    )
-    assert xacro_file is not None, "Failed to parse Xacro file"
-
-    doc = xacro.process_file(xacro_file)
-    robot_desc = doc.toxml()
-
-    robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="screen",
-        parameters=[{"use_sim_time": use_sim_time, "robot_description": robot_desc}],
+    robot_state_publisher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("soonduck_simulation"),
+                    "launch",
+                    "rsp.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "true"}.items(),
     )
 
     joint_state_publisher = Node(
@@ -93,86 +84,3 @@ def generate_launch_description():
             rviz2_launch,
         ]
     )
-
-
-# def generate_launch_description():
-#     # Launches Gazebo
-#     gazeboLaunch = IncludeLaunchDescription(
-#         PythonLaunchDescriptionSource(
-#             [
-#                 os.path.join(
-#                     get_package_share_directory("gazebo_ros"),
-#                     "launch",
-#                     "gazebo.launch.py",
-#                 )
-#             ]
-#         ),
-#         # launch_arguments={"world": world_path}.items(),
-#     )
-
-#     filenameURDF = "soonduck.urdf"
-#     # filenameYaml = "rov_description_params.yaml"
-
-#     # Path to Xacro file of robot
-#     path_to_urdf = os.path.join(
-#         get_package_share_directory("soonduck_simulation"), "urdf", filenameURDF
-#     )
-#     # with open(path_to_urdf, 'r') as infp:
-#     # //  robot_desc = infp.read()
-
-#     robot_desc: ParameterValue = ParameterValue(
-#         Command(["xacro ", path_to_urdf]),
-#         value_type=str
-#         # Command("xacro rov.xacro params_path:=rov_description_params.yaml")
-#     )
-
-#     params = {"robot_description": robot_desc}
-
-#     # Publishes the state of the robot
-#     robot_state_publisher_node = Node(
-#         package="robot_state_publisher",
-#         executable="robot_state_publisher",
-#         parameters=[params],
-#         arguments=[path_to_urdf],
-#     )
-
-#     # joint_state_publisher_node = Node(
-#     #     package="joint_state_publisher",
-#     #     executable="joint_state_publisher",
-#     #     arguments=[path_to_urdf],
-#     # )
-
-#     # spawn_entity_node = Node(
-#     #     package="gazebo_ros",
-#     #     executable="spawn_entity.py",
-#     #     arguments=[
-#     #         "-entity",
-#     #         "rov",
-#     #         "-topic",
-#     #         "/robot_description",
-#     #         "-x",
-#     #         "0",
-#     #         "-y",
-#     #         "2",
-#     #         "-z",
-#     #         "0",
-#     #         # 'output', 'screen',
-#     #     ],
-#     # )
-
-#     # rviz = Node(
-#     #     package="rviz2",
-#     #     executable="rviz2",
-#     #     arguments=['-d' + os.pazebo'), 'config', 'rov.rviz')],
-
-#     # )
-
-#     return LaunchDescription(
-#         [
-#             gazeboLaunch,
-#             robot_state_publisher_node,
-#             # joint_state_publisher_node,
-#             # spawn_entity_node,
-#             # rviz,
-#         ]
-#     )
