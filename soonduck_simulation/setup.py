745453e65f0e85fc712ae885296d3c295a543819
sxj754 <sxj754@case.edu>
2023-01-31 23:49:22 -0500

    Update setup.py

--- soonduck_simulation/setup.py
+++ soonduck_simulation/setup.py
@@ -13,7 +13,7 @@ setup(
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
         (os.path.join("share", package_name, "description"), glob("description/*")),
-        (os.path.join("share", package_name, "rviz"), glob("rviz/*.rviz")),
+        (os.path.join("share", package_name, "config"), glob("config/*")),
         (os.path.join("share", package_name, "worlds"), glob("worlds/*.world")),
     ],
     install_requires=["setuptools"],
